3a2413f05ba7a3c41c40d75d87c6b1d2f37e4263
Колпакова Полина <polinakolpakova1784@gmail.com>
2025-03-04 18:58:17 +0300

    Add files via upload

---
+++ лаб 7/лаб 7/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace лаб_7
+{
+    public class Plan
+    {
+        private List<Shape> shapes = new List<Shape>();
+        public void Add(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+        public void GetStatistics()
+        {
+            int RectanglesCount = 0;
+            int CirclesCount = 0;
+            int TrianglesCount = 0;
+            foreach (var shape in shapes)
+            {
+                if (shape is Rectangle)
+                {
+                    RectanglesCount++;
+                }
+                else if (shape is Circle)
+                {
+                    CirclesCount++;
+                }
+                else if (shape is Triangle)
+                {
+                    TrianglesCount++;
+                }
+            }
+        }
+    }
+    public class Shape
+    {
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+        public override string ToString()
+        {
+            Console.WriteLine($"Площадь: {Area}, Периметр: {Perimeter}");
+            return ($"Площадь: {Area}, Периметр: {Perimeter}");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public void area()
+        {
+            Area = Width * Height;
+        }
+        public void perimeter()
+        {
+            Perimeter = 2 * (Width + Height);
+        }
+        public override string ToString()
+        {
+            Console.WriteLine($"Прямоугольник Ширина: {Width}, Высота: {Height}, Площадь: {Area}, Периметр: {Perimeter}");
+            return $"Прямоугольник Ширина: {Width}, Высота: {Height}, Площадь: {Area}, Периметр: {Perimeter}";
+        }
+    }
+    public class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public void area()
+        {
+            Area = Math.PI * Radius * Radius;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine($"Круг Радиус: {Radius}, Площадь: {Area}, Периметр: {Perimeter}");
+            return $"Круг Радиус: {Radius}, Площадь: {Area}, Периметр: {Perimeter}";
+        }
+    }
+    public class Triangle : Shape
+    {
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+        public int SideC { get; set; }
+        public void perimeter()
+        {
+            Perimeter = SideA + SideB + SideC;
+        }
+        public void area()
+        {
+            double s = Perimeter / 2;
+            Area = Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+        public override string ToString()
+        {
+            Console.WriteLine($"Треугольник SideA: {SideA}, SideB: {SideB}, SideC: { SideC}, Площадь: { Area}, Периметр: { Perimeter}");
+        return $"Треугольник SideA: {SideA}, SideB: {SideB}, SideC: {SideC}, Площадь: {Area}, Периметр: { Perimeter}";
+        }
+    }
+    public class Program {
+        static void Main()
+        {
+            Plan NewPlan = new Plan();
+            Shape NewShape = new Shape { Area = 64, Perimeter = 128 };
+            NewShape.ToString();
+            Rectangle Rect = new Rectangle { Width = 3, Height = 5 };
+            Rect.area();
+            Rect.perimeter();
+            Rect.ToString();
+            Circle Circle = new Circle { Radius = 4 };
+            Circle.area();
+            Circle.ToString();
+            Circle Circle2 = new Circle { Radius = 6 };
+            Triangle Triangle = new Triangle { SideA = 3, SideB = 4, SideC = 5 };
+            Triangle.perimeter();
+            Triangle.area();
+            Triangle.ToString();
+            NewPlan.Add(Rect);
+            NewPlan.Add(Circle);
+            NewPlan.Add(Circle2);
+            NewPlan.Add(Triangle);
+            NewPlan.GetStatistics();
+        }
+    } 
+}
+
---
+++ лаб 7/лаб 7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("лаб 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("лаб 7")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b075bb08-5b68-4c69-bb7e-6dbae7046571")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
