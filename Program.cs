d2bfe14de976256c55df6b92ca0be411b36fedb6
Колпакова Полина <polinakolpakova1784@gmail.com>
2025-03-04 18:54:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Person
+{
+    public string name = "Ben";
+    public int age = 18;
+    public string email = "ben@gmail.com";
+    public Person(string name)
+    {
+        this.name = name;
+    }
+    public Person(string name, int age) : this(name)
+    {
+        this.age = age;
+    }
+    public Person(string name, int age, string email) :
+    this("Bob", age)
+    {
+        this.email = email;
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Alice", 25, "alice@example.com");
+        Console.WriteLine($"Name: {person.name}, Age: {person.age}, Email: {person.email}");
+    }
+}
